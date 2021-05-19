75cdebbcd403f5b616680adb494c8a3716892221
Phillip Thomas <phillip@paralleldomain.com>
2021-05-19 17:28:24 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
@@ -8,14 +8,15 @@ setup(
     version="0.0.2dev",
     author="Nisse Knudsen",
     author_email="nisse@paralleldomain.com",
-    packages=["paralleldomain"],
+    packages=find_packages(exclude=["test_paralleldomain"]),
+    package_data={"paralleldomain": ["py.typed"]},
     python_requires=">=3.6",
     long_description="Python SDK for ParallelDomain Datasets",
     install_requires=requirements,
     extras_require={
-            "dev": [
-                "pytest>=5.3.1",
-                "pytest-cov>=2.8.1",
-            ]
-        },
-)
+        "dev": [
+            "pytest>=5.3.1",
+            "pytest-cov>=2.8.1",
+        ]
+    },
+    zip_safe=False)
