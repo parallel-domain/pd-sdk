395d1d26ae2f270be60e8d068c29c1f079791338
phillip-thomas-pd <83581392+phillip-thomas-pd@users.noreply.github.com>
2021-06-17 11:01:15 +0200

    removed smart open since profiling showed it to be slower than the cloud lib default. Removed mocked AnyPath class and use CloudLib AnyPath instead. (#50)

--- paralleldomain/utilities/any_path.py
+++ paralleldomain/utilities/any_path.py
@@ -1,26 +1,5 @@
-import abc
-import os
-from pathlib import Path
-from typing import Union
-from typing.io import IO
+from cloudpathlib import AnyPath as CloudLibAnyPath
 
-from cloudpathlib import CloudPath
-from smart_open.smart_open_lib import open
-
-AdjustedCloudPath = CloudPath
-
-if os.environ.get("USESMARTOPEN", True):
-
-    def _open_wrap(self, mode, *args, **kwargs) -> IO:
-        return open(uri=self._str, mode=mode, *args, **kwargs)
-
-    AdjustedCloudPath.open = _open_wrap
-
-
-class AnyPath:
-    def __new__(cls, path: Union[str, AdjustedCloudPath, Path, CloudPath], *args, **kwargs):
-        # Dispatch to subclass if base CloudPath
-        str_path = str(path)
-        if str_path.startswith("s3") or str_path.startswith("gs") or str_path.startswith("azure"):
-            return AdjustedCloudPath(str_path, *args, **kwargs)
-        return Path(str_path, *args, **kwargs)
+# we import from here just in case we change away from cloud lib in the future
+# then we just need to replace the AnyPath Reference
+AnyPath = CloudLibAnyPath
