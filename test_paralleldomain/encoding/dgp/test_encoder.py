06f9d8f85e9f775ff31887e36f759c5008af5257
pd-nisse <nisse@paralleldomain.com>
2021-07-07 11:13:00 +0200

    Encoder test case

--- test_paralleldomain/encoding/dgp/test_encoder.py
+++ test_paralleldomain/encoding/dgp/test_encoder.py
@@ -3,24 +3,17 @@ from tempfile import TemporaryDirectory
 from paralleldomain import Dataset
 from paralleldomain.decoding.dgp.decoder import DGPDecoder
 from paralleldomain.encoding.dgp.encoder import DGPEncoder
+from paralleldomain.utilities.any_path import AnyPath
 
 
 def test_encoding_of_modified_scene(dataset: Dataset):
-    """
-    decoder = DGPDecoder(dataset_path=dataset_input_path)
-    dataset = Dataset.from_decoder(decoder=decoder)
+    output_path = AnyPath(TemporaryDirectory().name)
 
-    with DGPEncoder(dataset=dataset, output_path=dataset_output_path, frame_slicing=slice(0, 2)) as encoder:
+    # encoding
+    with DGPEncoder(dataset=dataset, output_path=output_path, frame_slice=slice(0, 2)) as encoder:
         with dataset.get_editable_scene(scene_name=dataset.scene_names[0]) as scene:
             encoder.encode_scene(scene)
 
-
-
-    with dataset.get_editable_scene(scene_name=dataset.scene_names[0]) as scene:
-        with TemporaryDirectory() as temp_dir:
-            encoder = DGPEncoder(output_path=temp_dir)
-            encoder.encode_dataset(dataset)
-
-            decoder = DGPDecoder(dataset_path=temp_dir)
-            decoded_dataset = Dataset.from_decoder(decoder=decoder)
-            decoded_scene = decoded_dataset.get_scene(scene_name=scene.name)"""
+    # decoding the encoded
+    decoder = DGPDecoder(dataset_path=output_path)
+    Dataset.from_decoder(decoder=decoder)
