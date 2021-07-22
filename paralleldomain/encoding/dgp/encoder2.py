c2f5b9f39f09c157a6d0685824a85b50efee25ec
pd-nisse <nisse@paralleldomain.com>
2021-07-22 21:30:37 +0200

    Writing point_cloud npz files

--- paralleldomain/encoding/dgp/encoder2.py
+++ paralleldomain/encoding/dgp/encoder2.py
@@ -1,5 +1,7 @@
 import argparse
 
+import numpy as np
+
 from paralleldomain.encoding.encoder import DatasetEncoder, SceneEncoder
 from paralleldomain.encoding.utils import fsio
 from paralleldomain.model.sensor import SensorFrame
@@ -9,17 +11,56 @@ class DGPSceneEncoder(SceneEncoder):
     def _encode_rgb(self, sensor_frame: SensorFrame):
         output_path = self._output_path / "rgb" / sensor_frame.sensor_name / f"{int(sensor_frame.frame_id):018d}.png"
         self._run_async(func=fsio.write_png, obj=sensor_frame.image.rgba, path=output_path)
+        return output_path
+
+    def _encode_point_cloud(self, sensor_frame: SensorFrame):
+        output_path = (
+            self._output_path / "point_cloud" / sensor_frame.sensor_name / f"{int(sensor_frame.frame_id):018d}.npz"
+        )
+
+        pc = sensor_frame.point_cloud
+        pc_dtypes = [
+            ("X", "<f4"),
+            ("Y", "<f4"),
+            ("Z", "<f4"),
+            ("INTENSITY", "<f4"),
+            ("R", "<f4"),
+            ("G", "<f4"),
+            ("B", "<f4"),
+            ("RING_ID", "<u4"),
+            ("TIMESTAMP", "<u8"),
+        ]
+
+        row_count = pc.length
+        pc_data = np.empty(row_count, dtype=pc_dtypes)
+
+        pc_data["X"] = pc.xyz[:, 0]
+        pc_data["Y"] = pc.xyz[:, 1]
+        pc_data["Z"] = pc.xyz[:, 2]
+        pc_data["INTENSITY"] = pc.intensity[:, 0]
+        pc_data["R"] = pc.rgb[:, 0]
+        pc_data["G"] = pc.rgb[:, 1]
+        pc_data["B"] = pc.rgb[:, 2]
+        pc_data["RING_ID"] = pc.ring[:, 0]
+        pc_data["TIMESTAMP"] = pc.ts[:, 0]
+
+        self._run_async(func=fsio.write_npz, data=pc_data)
+
+        return output_path
 
     def _encode_camera_frame(self, camera_frame: SensorFrame):
         self._encode_rgb(sensor_frame=camera_frame)
 
     def _encode_lidar_frame(self, lidar_frame: SensorFrame):
-        ...
+        self._encode_point_cloud(sensor_frame=lidar_frame)
 
     def _prepare_output_directories(self) -> None:
         super()._prepare_output_directories()
         for camera_name in self._scene.camera_names:
             (self._output_path / "rgb" / camera_name).mkdir(exist_ok=True, parents=True)
+
+        for lidar_name in self._scene.lidar_names:
+            (self._output_path / "point_cloud" / lidar_name).mkdir(exist_ok=True, parents=True)
 
 
 class DGPDatasetEncoder(DatasetEncoder):
