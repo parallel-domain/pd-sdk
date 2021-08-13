11d3c05f2ccb5003da8ddb4e7cd8ecd9b1c63de3
Phillip Thomas <phillip@paralleldomain.com>
2021-08-13 18:14:03 +0200

    fixed anypath tests

--- test_paralleldomain/utilities/test_any_path.py
+++ test_paralleldomain/utilities/test_any_path.py
@@ -3,13 +3,21 @@ from tempfile import TemporaryDirectory
 
 from cloudpathlib import CloudPath
 
-from paralleldomain.utilities.any_path import AnyPath
+from paralleldomain.utilities.any_path import AnyPath, S3Path
 
 
 def test_resolving():
     with TemporaryDirectory() as temp_dir:
         path = AnyPath(temp_dir)
         assert path.exists()
-        assert isinstance(path, Path)
+        assert isinstance(path._backend, Path)
         path = AnyPath("s3://paralleldomain-testing/")
-        assert isinstance(path, CloudPath)
+        assert isinstance(path._backend, S3Path)
+
+
+def test_concat():
+    with TemporaryDirectory() as temp_dir:
+        path = AnyPath(temp_dir) / "test"
+        assert isinstance(path, AnyPath)
+        path = AnyPath("s3://paralleldomain-testing/") / "test"
+        assert isinstance(path, AnyPath)
