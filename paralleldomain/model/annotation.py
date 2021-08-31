6111c9649825ef0c698bce2c459cef04a6941aa8
pd-nisse <nisse@paralleldomain.com>
2021-08-31 12:50:41 +0200

    Fixed order for vertices, edges, faces and docstrings

--- paralleldomain/model/annotation.py
+++ paralleldomain/model/annotation.py
@@ -7,16 +7,17 @@ import numpy as np
 
 from paralleldomain.model.transformation import Transformation
 
+# _UNIT_BOUNDING_BOX_3D = (CoordinateSystem("FLU") > INTERNAL_COORDINATE_SYSTEM).rotation_matrix @ np.array(
 _UNIT_BOUNDING_BOX_3D = np.array(
     [
-        [1, 1, -1],
-        [1, 1, 1],
-        [1, -1, 1],
-        [1, -1, -1],
-        [-1, 1, -1],
-        [-1, 1, 1],
-        [-1, -1, 1],
-        [-1, -1, -1],
+        [1, -1, -1],  # FRD
+        [1, -1, 1],  # FRU
+        [1, 1, 1],  # FLU
+        [1, 1, -1],  # FLD
+        [-1, -1, -1],  # BRD
+        [-1, -1, 1],  # BRU
+        [-1, 1, 1],  # BLU
+        [-1, 1, -1],  # BLD
     ]
 )  # CCW order of points for each face ( [0:4]: Front, [4:8]: Back )
 
@@ -103,8 +104,23 @@ class BoundingBox3D:
 
     @property
     def vertices(self) -> np.ndarray:
-        scaled_box = np.ones(shape=(_UNIT_BOUNDING_BOX_3D.shape[0], 4))
-        scaled_box[:, :3] = _UNIT_BOUNDING_BOX_3D * np.array(
+        """np.ndarray: Returns the 3D vertices of a bounding box.
+
+        Vertices are returned in the following order:
+
+           5--------6
+          /|   top /|
+         / |      / |
+        1--------2  |
+        |  4-----|--7
+        | /      | /
+        |/       |/ left
+        0--------3
+          front
+
+        """
+        scaled_vertices = np.ones(shape=(_UNIT_BOUNDING_BOX_3D.shape[0], 4))
+        scaled_vertices[:, :3] = _UNIT_BOUNDING_BOX_3D * np.array(
             [
                 self.length / 2,
                 self.width / 2,
@@ -112,26 +128,90 @@ class BoundingBox3D:
             ]
         )
 
-        transformed_box = (self.pose @ scaled_box.T).T[:, :3]
+        transformed_vertices = (self.pose @ scaled_vertices.T).T[:, :3]
 
-        return transformed_box
+        return transformed_vertices
 
     @property
     def edges(self) -> np.ndarray:
-        # vertices = self.vertices
-        return np.zeros(shape=(12, 2, 3))
+        """np.ndarray: Returns the 3D edges of a bounding box.
+
+        Edges are returned in order of connecting the vertices in the following way:
+
+           5--------6
+          /|   top /|
+         / |      / |
+        1--------2  |
+        |  4-----|--7
+        | /      | /
+        |/       |/ left
+        0--------3
+          front
+
+        - 0 -> 1
+        - 1 -> 2
+        - 2 -> 3
+        - 3 -> 0
+        - 4 -> 5
+        - 5 -> 6
+        - 6 -> 7
+        - 7 -> 4
+        - 2 -> 6
+        - 7 -> 3
+        - 1 -> 5
+        - 5 -> 0
+
+        """
+        vertices = self.vertices
+        edges = np.empty(shape=(12, 2, 3))
+
+        edges[0, :, :] = vertices[[0, 1], :]  # FRD -> FRU (0 -> 1)
+        edges[1, :, :] = vertices[[1, 2], :]  # FRU -> FLU (1 -> 2)
+        edges[2, :, :] = vertices[[2, 3], :]  # FLU -> FLD (2 -> 3)
+        edges[3, :, :] = vertices[[3, 0], :]  # FLD -> FRD (3 -> 0)
+        edges[4, :, :] = vertices[[4, 5], :]  # BRD -> BRU (4 -> 5)
+        edges[5, :, :] = vertices[[5, 6], :]  # BRU -> BLU (5 -> 6)
+        edges[6, :, :] = vertices[[6, 7], :]  # BLU -> BLD (6 -> 7)
+        edges[7, :, :] = vertices[[7, 4], :]  # BLD -> BRD (7 -> 4)
+        edges[8, :, :] = vertices[[2, 6], :]  # FLU -> BLU (2 -> 6)
+        edges[9, :, :] = vertices[[7, 3], :]  # BLD -> FLD (7 -> 3)
+        edges[10, :, :] = vertices[[1, 5], :]  # FRU -> BRU (1 -> 5)
+        edges[11, :, :] = vertices[[4, 0], :]  # BRD -> FRD (5 -> 0)
+
+        return edges
 
     @property
     def faces(self) -> np.ndarray:
+        """np.ndarray: Returns the 3D faces of a bounding box.
+
+        Faces are returned in order of connecting the vertices in the following way:
+
+           5--------6
+          /|   top /|
+         / |      / |
+        1--------2  |
+        |  4-----|--7
+        | /      | /
+        |/       |/ left
+        0--------3
+          front
+
+        - 0 -> 1 -> 2 -> 3 (front)
+        - 4 -> 5 -> 6 -> 7 (back)
+        - 3 -> 2 -> 6 -> 7 (left)
+        - 0 -> 1 -> 5 -> 4 (right)
+        - 6 -> 2 -> 1 -> 5 (top)
+        - 7 -> 3 -> 0 -> 4 (bottom)
+        """
         vertices = self.vertices
         faces = np.empty(shape=(6, 4, 3))
 
-        faces[0, :, :] = vertices[:4, :]
-        faces[1, :, :] = vertices[4:, :]
-        faces[2, :, :] = vertices[[0, 1, 5, 4], :]
-        faces[3, :, :] = vertices[[3, 2, 6, 7], :]
-        faces[4, :, :] = vertices[[0, 3, 7, 4], :]
-        faces[5, :, :] = vertices[[1, 2, 6, 5], :]
+        faces[0, :, :] = vertices[[0, 1, 2, 3], :]  # front
+        faces[1, :, :] = vertices[[4, 5, 6, 7], :]  # back
+        faces[2, :, :] = vertices[[3, 2, 6, 7], :]  # left
+        faces[3, :, :] = vertices[[0, 1, 5, 4], :]  # right
+        faces[4, :, :] = vertices[[6, 2, 1, 5], :]  # up (top)
+        faces[5, :, :] = vertices[[7, 3, 0, 4], :]  # down (bottom)
 
         return faces
 
