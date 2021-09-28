d77556f623e034d065fa71ae2ac57ea519efba1d
Phillip Thomas <phillip@paralleldomain.com>
2021-09-28 13:37:14 +0200

    limit default numages data usage to 2.0 GB

--- paralleldomain/decoding/nuimages/common.py
+++ paralleldomain/decoding/nuimages/common.py
@@ -31,10 +31,10 @@ def load_table(dataset_root: AnyPath, split_name: str, table_name: str) -> List[
     raise ValueError(f"Error: Table {table_name} does not exist!")
 
 
-_NU_IMAGES_DATA_MAX_SIZE = 1.6e9  # GB
+_NU_IMAGES_DATA_MAX_SIZE = 2.0e9  # GB
 cache_max_ram_usage_factor = float(
     os.environ.get("NU_CACHE_MAX_USAGE_FACTOR", _NU_IMAGES_DATA_MAX_SIZE / psutil.virtual_memory().total)
-)  # use 1.6 GB by default
+)  # use 2.0 GB by default
 ram_keep_free_factor = float(os.environ.get("NU_CACHE_KEEP_FREE_FACTOR", 0.05))  # 5% have to stay free
 
 NU_IM_DATA_CACHE = None
