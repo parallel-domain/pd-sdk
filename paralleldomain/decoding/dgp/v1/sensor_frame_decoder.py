824856993d4d1541265ff84e09056817b4d8b38a
pd-nisse <nisse@paralleldomain.com>
2021-11-04 15:38:37 +0100

    Fixed incorrect filtering if Datum is of type image or point_cloud

--- paralleldomain/decoding/dgp/v1/sensor_frame_decoder.py
+++ paralleldomain/decoding/dgp/v1/sensor_frame_decoder.py
@@ -437,6 +437,8 @@ class DGPCameraSensorFrameDecoder(DGPSensorFrameDecoder, CameraSensorFrameDecode
             camera_model = CameraModel.OPENCV_FISHEYE
         elif dto.fisheye is False or dto.fisheye == 0:
             camera_model = CameraModel.OPENCV_PINHOLE
+        elif dto.fisheye == 3:
+            camera_model = CameraModel.PD_FISHEYE
         elif dto.fisheye > 1:
             camera_model = f"custom_{dto.fisheye}"
 
