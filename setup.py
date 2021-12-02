6499c2eb59cdbdbf74a754caa3e15f1c18019536
Michael Stanley <michael.stanley@paralleldomain.com>
2021-12-02 09:30:11 -0800

    increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="paralleldomain",
-    version="0.5.1",
+    version="0.5.2",
     author=["Nisse Knudsen", "Phillip Thomas", "Lars Pandikow", "Michael Stanley"],
     author_email=[
         "nisse@paralleldomain.com",
