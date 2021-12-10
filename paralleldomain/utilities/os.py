218fa0b62b235c42a4a85b63522ff3f56faf63c7
pd-nisse <nisse@paralleldomain.com>
2021-12-10 22:07:45 +0100

    Helpers to calculate memory + cpu correctly across host and container systems

--- paralleldomain/utilities/os.py
+++ paralleldomain/utilities/os.py
@@ -1,25 +1,58 @@
 import os
+import sys
+from dataclasses import dataclass
+
+import psutil
+from cgroupspy.trees import Tree
+
+from paralleldomain.utilities.any_path import AnyPath
 
 
 def is_container() -> bool:
-    path = "/proc/self/cgroup"
-    return os.path.exists("/.dockerenv") or os.path.isfile(path) and any("docker" in line for line in open(path))
+    """Returns `True` if process is running inside a (Docker) container."""
+    cgroup_path = AnyPath("/proc/self/cgroup")
+    return (
+        AnyPath("/.dockerenv").exists()
+        or cgroup_path.is_file()
+        and any("docker" in line for line in cgroup_path.open(mode="r"))
+    )
 
 
 def cpu_count() -> int:
+    """Returns CPU count. If process is running inside a (Docker) container,
+    it returns the allocated virtual CPU count."""
     if is_container():
-        if os.path.isfile("/sys/fs/cgroup/cpu/cpu.cfs_quota_us"):
-            cpu_quota = int(open("/sys/fs/cgroup/cpu/cpu.cfs_quota_us").read().rstrip())
-        # print(cpu_quota) # Not useful for AWS Batch based jobs as result is -1, but works on local linux systems
-        if cpu_quota != -1 and os.path.isfile("/sys/fs/cgroup/cpu/cpu.cfs_period_us"):
-            cpu_period = int(open("/sys/fs/cgroup/cpu/cpu.cfs_period_us").read().rstrip())
-            # print(cpu_period)
-            avail_cpu = int(cpu_quota / cpu_period)
-            # Divide quota by period and you should get num of allotted CPU to the container,rounded down if fractional.
-        elif os.path.isfile("/sys/fs/cgroup/cpu/cpu.shares"):
-            cpu_shares = int(open("/sys/fs/cgroup/cpu/cpu.shares").read().rstrip())
-            # print(cpu_shares) # For AWS, gives correct value * 1024.
-            avail_cpu = int(cpu_shares / 1024)
-        return avail_cpu
+        cgroup_tree = Tree()
+        cpu_controller = cgroup_tree.get_node_by_path("/cpu/").controller
+        if cpu_controller.cfs_quota_us is None:  # running inside AWS-hosted container
+            return int(cpu_controller.shares / 1024)
+        else:  # running locally hosted container
+            return int(cpu_controller.cfs_quota_us / cpu_controller.cfs_period_us)
     else:
         return os.cpu_count()
+
+
+@dataclass
+class Memory:
+    total: int
+    used: int
+
+    @property
+    def available(self) -> int:
+        return self.total - self.used
+
+
+def virtual_memory() -> Memory:
+    """Returns total, used and available memory. If process is running inside a (Docker) container,
+    it returns the allocated memory limit, or pyhsical host memory if no explicit limit has been set."""
+    if is_container():
+        cgroup_tree = Tree()
+        memory_controller = cgroup_tree.get_node_by_path("/memory/").controller
+
+        memory_limit = memory_controller.limit_in_bytes
+        if memory_limit == (sys.maxsize - sys.maxsize % 4096):  # no container limit - take host physical limit
+            memory_limit = psutil.virtual_memory().total
+        return Memory(total=memory_limit, used=memory_controller.usage_in_bytes)
+    else:
+        psutil_vm = psutil.virtual_memory()
+        return Memory(total=psutil_vm.total, used=psutil_vm.used)
