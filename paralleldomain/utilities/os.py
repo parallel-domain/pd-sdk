5faef4fb3947c0b0612187a730d8e44a81d330c2
pd-nisse <nisse@paralleldomain.com>
2021-12-17 14:50:42 +0100

    Removed virtual_memory calculation for containers

--- paralleldomain/utilities/os.py
+++ paralleldomain/utilities/os.py
@@ -1,9 +1,6 @@
 import os
-import sys
-from dataclasses import dataclass
 from pathlib import Path
 
-import psutil
 from cgroupspy.trees import Tree
 
 
@@ -29,29 +26,3 @@ def cpu_count() -> int:
             return int(cpu_controller.cfs_quota_us / cpu_controller.cfs_period_us)
     else:
         return os.cpu_count()
-
-
-@dataclass
-class Memory:
-    total: int
-    used: int
-
-    @property
-    def available(self) -> int:
-        return self.total - self.used
-
-
-def virtual_memory() -> Memory:
-    """Returns total, used and available memory. If process is running inside a (Docker) container,
-    it returns the allocated memory limit, or pyhsical host memory if no explicit limit has been set."""
-    if is_container():
-        cgroup_tree = Tree()
-        memory_controller = cgroup_tree.get_node_by_path("/memory/").controller
-
-        memory_limit = memory_controller.limit_in_bytes
-        if memory_limit == (sys.maxsize - sys.maxsize % 4096):  # no container limit - take host physical limit
-            memory_limit = psutil.virtual_memory().total
-        return Memory(total=memory_limit, used=memory_controller.usage_in_bytes)
-    else:
-        psutil_vm = psutil.virtual_memory()
-        return Memory(total=psutil_vm.total, used=psutil_vm.used)
