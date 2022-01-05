a7ca450feac2ad61de29f59d2c9d0761baf8c17e
phillip-thomas-pd <83581392+phillip-thomas-pd@users.noreply.github.com>
2022-01-05 09:30:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="paralleldomain",
-    version="0.5.3",
+    version="0.5.6",
     author=["Nisse Knudsen", "Phillip Thomas", "Lars Pandikow", "Michael Stanley"],
     author_email=[
         "nisse@paralleldomain.com",
