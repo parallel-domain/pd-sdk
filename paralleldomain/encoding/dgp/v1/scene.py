8e36bb424a69d3e30e8f1b1f90df717cd6eb32ab
Phillip Thomas <phillip@paralleldomain.com>
2022-01-28 11:15:01 +0100

    fixed _encode_key_point_2d to use point not points parameter

--- paralleldomain/encoding/dgp/v1/scene.py
+++ paralleldomain/encoding/dgp/v1/scene.py
@@ -429,7 +429,7 @@ class DGPSceneEncoder(SceneEncoder):
         keypoint_proto = annotations_pb2.KeyPoint2DAnnotation(
             class_id=point.class_id,
             attributes={_attribute_key_dump(k): _attribute_value_dump(v) for k, v in point.attributes.items()},
-            points=annotations_pb2.KeyPoint2D(x=point.x, y=point.y),
+            point=annotations_pb2.KeyPoint2D(x=point.x, y=point.y),
         )
 
         return keypoint_proto
