e825451ffdf30a854f435cb3d4f412b6c147ee28
pd-nisse <nisse@paralleldomain>
2022-04-28 11:21:04 -0700

    Removed submodule approach and change to flat repo checkout

--- paralleldomain/common/dgp/v1/update_protos.sh
+++ paralleldomain/common/dgp/v1/update_protos.sh
@@ -4,17 +4,15 @@
 # https://stackoverflow.com/a/60562182/628496
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR/src" || exit
+cd "$SCRIPT_DIR" || exit
+rm -rf src
 
-find . -type d -exec rm "{}/__init__.py" \;
-
-git stash
-git clean -fd
-git reset --hard
-git pull git@github.com:TRI-ML/dgp.git master --rebase
+git clone git@github.com:TRI-ML/dgp.git src
+cd src || exit
 git-filter-repo --path dgp/proto --path dgp/contribs/pd --force
 
 find . -type f -iname "*_pb2.py" -exec sed -i 's/from dgp.proto/from paralleldomain.common.dgp.v1.src.dgp.proto/g' {} \;
-
 find . -type d -exec touch "{}/__init__.py" \;
+
+rm -rf .git
 
