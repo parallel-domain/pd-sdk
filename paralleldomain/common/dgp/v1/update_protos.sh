5de36a294e75c1f9bef45ddd64d1fb004f9efcb9
pd-nisse <nisse@paralleldomain>
2022-04-28 10:53:00 -0700

    Updated to not moving paths inside submodule but leaving structure intact to avoid merge conflicts on every update

--- paralleldomain/common/dgp/v1/update_protos.sh
+++ paralleldomain/common/dgp/v1/update_protos.sh
@@ -4,14 +4,17 @@
 # https://stackoverflow.com/a/60562182/628496
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR/dgp" || exit
+cd "$SCRIPT_DIR/src" || exit
 
 find . -type d -exec rm "{}/__init__.py" \;
 
-git pull git@github.com:TRI-ML/dgp.git master --force
-git-filter-repo --path dgp/proto --path dgp/contribs/pd --path-rename dgp/proto:proto --path-rename dgp/contribs/pd:contribs/pd --force
+git stash
+git clean -fd
+git reset --hard
+git pull git@github.com:TRI-ML/dgp.git master --rebase
+git-filter-repo --path dgp/proto --path dgp/contribs/pd --force
 
-find . -type f -iname "*_pb2.py" -exec sed -i 's/from dgp.proto/from paralleldomain.common.dgp.v1.dgp.proto/g' {} \;
+find . -type f -iname "*_pb2.py" -exec sed -i 's/from dgp.proto/from paralleldomain.common.dgp.v1.src.dgp.proto/g' {} \;
 
 find . -type d -exec touch "{}/__init__.py" \;
 
