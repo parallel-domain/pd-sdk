cbd8d21f61080acf27538be42243e951287cca3e
pd-nisse <nisse@paralleldomain>
2022-04-29 11:25:13 -0700

    Disable Flake8 linter on existing and updated files

--- paralleldomain/common/dgp/v1/update_protos.sh
+++ paralleldomain/common/dgp/v1/update_protos.sh
@@ -11,7 +11,13 @@ git clone git@github.com:TRI-ML/dgp.git src
 cd src || exit
 git-filter-repo --path dgp/proto --path dgp/contribs/pd --force
 
+# disable Flake8 linting on pre-built python files
+find . -type f -iname "*_pb2.py" -exec sed -i '1s/^/# flake8: noqa \n/' {} \;
+find . -type f -iname "*_pb2_grpc.py" -exec sed -i '1s/^/# flake8: noqa \n/' {} \;
+
+# change relative to absolute imports
 find . -type f -iname "*_pb2.py" -exec sed -i 's/from dgp.proto/from paralleldomain.common.dgp.v1.src.dgp.proto/g' {} \;
+# make every folder a submodule
 find . -type d -exec touch "{}/__init__.py" \;
 
 rm -rf .git
