4495e33e0a4a5c6dfd50b2f76d809241dc9509bd
pd-nisse <nisse@paralleldomain>
2022-06-29 12:11:34 -0700

    Orthographic camera support in DGP Decoder, DGP Encoder, PD SDK Model and projection functions (2D -> 3D)

--- paralleldomain/encoding/dgp/scene.py
+++ paralleldomain/encoding/dgp/scene.py
@@ -61,6 +61,7 @@ class DGPSceneEncoder(SceneEncoder):
             CameraModel.OPENCV_PINHOLE: 0,
             CameraModel.OPENCV_FISHEYE: 1,
             CameraModel.PD_FISHEYE: 3,
+            CameraModel.PD_ORTHOGRAPHIC: 6,
         },
     )
 
