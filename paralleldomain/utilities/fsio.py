a3db993c98eb1a28b82386e99d3eab1a07b61bf5
Phillip Thomas <phillip@paralleldomain.com>
2022-07-01 17:50:45 +0200

    unordered scene decoder for waymo open dataset. Currently only supports camera images, semseg and isntance seg

--- paralleldomain/utilities/fsio.py
+++ paralleldomain/utilities/fsio.py
@@ -77,19 +77,24 @@ def read_image(path: AnyPath, convert_to_rgb: bool = True, is_indexed=False) -> 
         if is_indexed:
             pil_image = Image.open(BytesIO(fp.read()))
             return np.asarray(pil_image)
-        image_data = cv2.imdecode(
-            buf=np.frombuffer(fp.read(), np.uint8),
-            flags=cv2.IMREAD_UNCHANGED,
-        )
-        if convert_to_rgb:
-            color_convert_code = cv2.COLOR_BGR2RGB
-            if image_data.shape[-1] == 4:
-                color_convert_code = cv2.COLOR_BGRA2RGBA
+        else:
+            return read_image_bytes(images_bytes=fp.read(), convert_to_rgb=convert_to_rgb)
 
-            image_data = cv2.cvtColor(
-                src=image_data,
-                code=color_convert_code,
-            )
+
+def read_image_bytes(images_bytes: bytes, convert_to_rgb: bool = True) -> np.ndarray:
+    image_data = cv2.imdecode(
+        buf=np.frombuffer(images_bytes, np.uint8),
+        flags=cv2.IMREAD_UNCHANGED,
+    )
+    if convert_to_rgb:
+        color_convert_code = cv2.COLOR_BGR2RGB
+        if image_data.shape[-1] == 4:
+            color_convert_code = cv2.COLOR_BGRA2RGBA
+
+        image_data = cv2.cvtColor(
+            src=image_data,
+            code=color_convert_code,
+        )
     return image_data
 
 
