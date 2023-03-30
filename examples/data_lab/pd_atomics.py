0fc0d1fd021c025e857fb6a61882be44f6143877
PD Buildbot <buildbot@paralleldomain.com>
2023-03-30 04:18:28 +0000

    Update for version v0.9.0

--- examples/data_lab/pd_atomics.py
+++ examples/data_lab/pd_atomics.py
@@ -1,9 +1,15 @@
 import logging
 import random
 
+from pd.assets import ObjAssets, init_asset_registry_version
 from pd.data_lab.config.distribution import CenterSpreadConfig, EnumDistribution, MinMaxConfigInt
 from pd.data_lab.render_instance import RenderInstance
 from pd.data_lab.sim_instance import SimulationInstance
+from pd.internal.proto.keystone.generated.wrapper.pd_sensor_pb2 import (
+    CameraIntrinsic,
+    DistortionParams,
+    SensorExtrinsic,
+)
 
 import paralleldomain.data_lab as data_lab
 from paralleldomain.data_lab.generators.debris import DebrisGeneratorParameters
@@ -23,16 +29,34 @@ from paralleldomain.utilities.transformation import Transformation
 setup_loggers(logger_names=["__main__", "paralleldomain", "pd"])
 logging.getLogger("pd.state.serialize").setLevel(logging.CRITICAL)
 
+init_asset_registry_version("v2.0.0-beta")
+
+
+def get_debris_asset_list() -> str:
+    # query specific assets from PD's asset registry and use as csv input for Debris generator
+    asset_objs = ObjAssets.select().where(
+        (ObjAssets.name % "*trash*") & (ObjAssets.width * ObjAssets.height * ObjAssets.length < 1.0)
+    )
+    asset_names = [o.name for o in asset_objs]
+    return ",".join(asset_names)
+
 
 sensor_rig = data_lab.SensorRig(
     sensor_configs=[
-        data_lab.SensorConfig.create_camera_sensor(
-            name="Front",
-            width=1920,
-            height=1080,
-            field_of_view_degrees=70,
-            pose=Transformation.from_euler_angles(
-                angles=[0.0, 0.0, 0.0], order="xyz", degrees=True, translation=[0.0, 0.0, 2.0]
+        data_lab.SensorConfig(
+            display_name="Front",
+            camera_intrinsic=CameraIntrinsic(
+                width=1920,
+                height=1080,
+                fov=70.0,
+            ),
+            sensor_extrinsic=SensorExtrinsic(
+                roll=0.0,
+                pitch=0.0,
+                yaw=0.0,
+                x=0.0,
+                y=0.0,
+                z=2.0,
             ),
         )
     ]
@@ -46,10 +70,11 @@ scenario.random_seed = random.randint(1, 1_000_000)  # set to a fixed integer to
 scenario.environment.time_of_day.set_category_weight(data_lab.TimeOfDays.Day, 1.0)
 scenario.environment.clouds.set_constant_value(0.5)
 scenario.environment.rain.set_constant_value(0.0)
+scenario.environment.fog.set_uniform_distribution(min_value=0.1, max_value=0.3)
 scenario.environment.wetness.set_uniform_distribution(min_value=0.1, max_value=0.3)
 
 # Select an environment
-scenario.set_location(data_lab.Location(name="SF_6thAndMission_medium", version="v2.0.1"))
+scenario.set_location(data_lab.Location(name="SF_6thAndMission_medium", version="v2.0.0-beta"))
 
 # Place ourselves in the world
 scenario.add_ego(
@@ -68,8 +93,10 @@ scenario.add_ego(
 
 scenario.add_objects(
     generator=DebrisGeneratorParameters(
-        max_debris_distance=25.0,
-        debris_asset_tag="trash_bottle_tall_01",
+        spawn_probability=0.5,
+        min_debris_distance=0.0,
+        max_debris_distance=30.0,
+        debris_asset_tag="trash_wrapper_01,trash_tobacco_01,trash_straw_plastic_01,trash_square_bottle_01",
         position_request=PositionRequest(
             location_relative_position_request=LocationRelativePositionRequest(
                 agent_tags=[SpecialAgentTag.EGO],
@@ -84,7 +111,7 @@ scenario.add_agents(
         position_request=PositionRequest(
             location_relative_position_request=LocationRelativePositionRequest(
                 agent_tags=[SpecialAgentTag.EGO],
-                max_spawn_radius=25.0,
+                max_spawn_radius=100.0,
             )
         ),
     )
@@ -96,31 +123,18 @@ scenario.add_agents(
         position_request=PositionRequest(
             location_relative_position_request=LocationRelativePositionRequest(
                 agent_tags=[SpecialAgentTag.EGO],
-                max_spawn_radius=200.0,
+                max_spawn_radius=100.0,
             )
         ),
     )
 )
 
-scenario.add_agents(
-    generator=RandomPedestrianGeneratorParameters(
-        num_of_pedestrians_range=MinMaxConfigInt(min=25, max=35),
-        position_request=PositionRequest(
-            location_relative_position_request=LocationRelativePositionRequest(
-                agent_tags=[SpecialAgentTag.EGO],
-                max_spawn_radius=200.0,
-            )
-        ),
-    )
-)
-
-# scenario.save_scenario(path=AnyPath("my_scenario.json"))
 
 data_lab.preview_scenario(
     scenario=scenario,
     frames_per_scene=100,
     show_image_for_n_seconds=1,
-    # sim_capture_rate=2,
-    sim_instance=SimulationInstance(address="ssl://sim.step-api-dev.paralleldomain.com:3014"),
-    render_instance=RenderInstance(address="ssl://ig.step-api-dev.paralleldomain.com:3014"),
+    sim_capture_rate=10,
+    sim_instance=SimulationInstance(address="ssl://sim.step-api-dev.paralleldomain.com:3001"),
+    render_instance=RenderInstance(address="ssl://ig.step-api-dev.paralleldomain.com:3001"),
 )
